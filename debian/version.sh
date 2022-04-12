a703534b0f3a2e426fd09cc9d537eaee29a74532
Joe Garcia <joe@joe-garcia.com>
2022-04-12 10:06:34 -0400

    Initial commit

---
+++ debian/version.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+main() {
+    # The version of the package
+    curr=$(awk -F'[[:blank:],]' '/Version/ {print $2}' DEBIAN/control)
+    # Parse the version number out of the package version
+    ver=$(echo "$curr" | awk -F'-' '{print $1}')
+    # Parse the release number out of the package version
+    rel=$(echo "$curr" | awk -F'-' '{print $2}')
+    # Bump the version
+    new="$ver-"$((rel+1))
+    # Replace the version in the control file
+    sed -i "s/^Version:.*/Version: $new/" DEBIAN/control
+    # Report the old and new version
+    echo "Bumped version from $curr to $new"
+}
